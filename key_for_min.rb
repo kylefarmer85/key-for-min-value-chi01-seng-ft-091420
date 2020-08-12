8e2de913cb59970953b3fba4d6cd01b22e685b98
Kyle Farmer <kylefarmer85@gmail.com>
2020-08-12 20:13:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   min_value = nil
   name_hash.each do |key, value|
-    if value < min_value
+    if value == nil || value < min_value
       min_value = value
       lowest_key = key
     end
